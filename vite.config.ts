9bd4c4c77974d85295cb7f7f2dc702a1a5edfbe0
amalm2002 <amalm2495@gmail.com>
2025-09-09 15:59:53 +0530

    chor :change the congfig file on vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,17 @@
 
 
 
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
-})
+  build: {
+    outDir: 'dist',
+  },
+  base: './', 
+});
